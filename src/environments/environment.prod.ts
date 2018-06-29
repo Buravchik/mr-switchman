8a6ffa3d1bf928b90bb6a56b30939989b11fa516
Sakvar <sakvar@gmail.com>
2018-06-29 23:43:00 +0300

    initial version from angular-fire

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyByN9ARax-IAwjywC-GDkcdournVuEwdPo",
+    authDomain: "japs-716bd.firebaseapp.com",
+    databaseURL: "https://japs-716bd.firebaseio.com",
+    projectId: "japs-716bd",
+    storageBucket: "japs-716bd.appspot.com",
+    messagingSenderId: "749070264992"
+  }
+};
